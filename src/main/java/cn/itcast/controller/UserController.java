3886d60c8e548aa2a8db889bc90b9246832b4993
waking777 <756274132@qq.com>
2019-12-27 17:22:28 +0800

    文件上传，uuid避免文件覆盖

--- src/main/java/cn/itcast/controller/UserController.java
+++ src/main/java/cn/itcast/controller/UserController.java
@@ -1,6 +1,9 @@
 package cn.itcast.controller;
 
 import cn.itcast.domain.User;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +13,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/testString")
-    public String testString(Model model){
+    public String testString(Model model) {
         System.out.println("testString执行了");
         //模拟从数据库中查询中User对象
         User user = new User();
@@ -24,7 +30,7 @@ public class UserController {
         user.setPassword("123");
         user.setAge(18);
         //model对象
-        model.addAttribute("user",user);
+        model.addAttribute("user", user);
         return "success";
     }
 
@@ -32,10 +38,10 @@ public class UserController {
     public void testVoid(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("testVoid执行了");
         //转发,一次请求
-        request.getRequestDispatcher("//WEB-INF/pages/success.jsp").forward(request,response);
+        request.getRequestDispatcher("//WEB-INF/pages/success.jsp").forward(request, response);
 
         //重定向
-        response.sendRedirect(request.getContextPath()+"index.jsp");
+        response.sendRedirect(request.getContextPath() + "index.jsp");
 
         //解决中文乱码
         response.setCharacterEncoding("UTF-8");
@@ -48,14 +54,14 @@ public class UserController {
 
 
     @RequestMapping("/testModelAndView")
-    public ModelAndView testModelAndView(){
+    public ModelAndView testModelAndView() {
         ModelAndView mv = new ModelAndView();
         User user = new User();
         user.setUsername("zhoudongyu");
         user.setPassword("999");
         user.setAge(18);
 
-        mv.addObject("user",user);
+        mv.addObject("user", user);
         mv.setViewName("success");
 
         return mv;
@@ -63,10 +69,12 @@ public class UserController {
 
     /**
      * 模拟异步请求响应
+     *
      * @param user
      */
     @RequestMapping("/testAjax")
-    public @ResponseBody User testAjax(@RequestBody User user) {
+    public @ResponseBody
+    User testAjax(@RequestBody User user) {
         System.out.println("testAjax方法执行了");
         //客户端发了ajax请求，传的是json字符串，后端吧json字符串封装到user对象中
         System.out.println(user);
@@ -76,5 +84,45 @@ public class UserController {
         user.setPassword("111");
         return user;
 
+    }
+
+    @RequestMapping("/fileupload1")
+    public String fileupload1(HttpServletRequest request) throws Exception {
+        System.out.println("文件上传");
+
+        //使用fileupload组件来完成文件上传
+        //上传的位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads");
+        //该路径是否存在
+        File file = new File(path);
+        if (!file.exists()) {
+            //不存在则创建改文件夹
+            file.mkdirs();
+        }
+        //解析request对象，获取上传文件项
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        //解析request
+        List<FileItem> items = upload.parseRequest(request);
+        //遍历文件项
+        for (FileItem item : items) {
+            //判断当前对象是否是上传文件项
+            if (item.isFormField()) {
+                //说明是普通表单项
+            } else {
+                //说明是上传文件项
+                //获取上传文件的名称
+                String filename = item.getName();
+                //吧文件名称设置为唯一值
+                String uuid = UUID.randomUUID().toString().replace("-", "");
+                filename = uuid + "_" + filename;
+                //完成文件上传
+                item.write(new File(path, filename));
+                //删除临时文件
+                item.delete();
+            }
+        }
+
+        return "success";
     }
 }
---
+++ src/main/webapp/fileupload.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+    <h3>文件上传</h3>
+
+    <form action="user/fileupload1" method="post" enctype="multipart/form-data">
+        选择文件：<input type="file" name="upload" /><br/>
+        <input type="submit" value="上传" />
+    </form>
+
+</body>
+</html>
